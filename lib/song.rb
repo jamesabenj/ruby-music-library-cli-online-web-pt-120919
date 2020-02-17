8d0888d7dee26312d791ba401a4bbab0f7b24296
James Benjamin <james.abenj@gmail.com>
2020-02-17 02:57:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,10 @@ class Song
 
   @@all =[]
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @artist = artist
     @name = name
+    @genre = genre
   end
 
   def self.all
